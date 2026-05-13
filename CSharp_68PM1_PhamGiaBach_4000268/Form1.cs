1d6ee131de910a721138643f3d92ce7c553bdfd5
PC <PC@LAPTOP-22L67JER>
2026-05-13 14:50:50 +0700

    Chức năng đăng nhập

---
+++ CSharp_68PM1_PhamGiaBach_4000268/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSharp_68PM1_PhamGiaBach_4000268
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+
+            string studentEmail = "bach4000268@st.huce.edu.vn";
+            string studentMSSV = "4000268";
+
+            if (email == studentEmail && password == studentMSSV)
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại");
+            }
+        }
+    }
+}
