a3acbb9116604e59c87a8fa0f1d35242eec057a5
bachpham0312 <bachpham0312@gmail.com>
2026-05-15 14:58:13 +0700

    Add UI QLSV

---
+++ CSharp_68PM1_PhamGiaBach_4000268/QuanLySinhVien.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CSharp_68PM1_PhamGiaBach_4000268
+{
+    partial class QuanLySinhVien
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.quảnLýSinhViênToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýLớpHọcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đăngXuấtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelMain = new System.Windows.Forms.Panel();
+
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quảnLýSinhViênToolStripMenuItem,
+            this.quảnLýLớpHọcToolStripMenuItem,
+            this.đăngXuấtToolStripMenuItem});
+
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1200, 28);
+
+            this.quảnLýSinhViênToolStripMenuItem.Name = "quảnLýSinhViênToolStripMenuItem";
+            this.quảnLýSinhViênToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.quảnLýSinhViênToolStripMenuItem.Text = "Quản Lý Sinh Viên";
+            this.quảnLýSinhViênToolStripMenuItem.Click += new System.EventHandler(this.quảnLýSinhViênToolStripMenuItem_Click);
+
+            this.quảnLýLớpHọcToolStripMenuItem.Name = "quảnLýLớpHọcToolStripMenuItem";
+            this.quảnLýLớpHọcToolStripMenuItem.Size = new System.Drawing.Size(144, 24);
+            this.quảnLýLớpHọcToolStripMenuItem.Text = "Quản Lý Lớp Học";
+            this.quảnLýLớpHọcToolStripMenuItem.Click += new System.EventHandler(this.quảnLýLớpHọcToolStripMenuItem_Click);
+
+            this.đăngXuấtToolStripMenuItem.Name = "đăngXuấtToolStripMenuItem";
+            this.đăngXuấtToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.đăngXuấtToolStripMenuItem.Text = "Đăng xuất";
+            this.đăngXuấtToolStripMenuItem.Click += new System.EventHandler(this.đăngXuấtToolStripMenuItem_Click);
+
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 28);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(1200, 700);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 728);
+
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.menuStrip1);
+
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "QuanLySinhVien";
+            this.Text = "Quản Lý Sinh Viên";
+
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýSinhViênToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýLớpHọcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem đăngXuấtToolStripMenuItem;
+        private System.Windows.Forms.Panel panelMain;
+    }
+}
---
+++ CSharp_68PM1_PhamGiaBach_4000268/QuanLySinhVien.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CSharp_68PM1_PhamGiaBach_4000268
+{
+    public partial class QuanLySinhVien : Form
+    {
+        public QuanLySinhVien()
+        {
+            InitializeComponent();
+
+            TaoGiaoDienSinhVien();
+        }
+
+        private void TaoGiaoDienSinhVien()
+        {
+            Label lblMaSV = new Label();
+            lblMaSV.Text = "Mã sinh viên";
+            lblMaSV.Location = new Point(30, 60);
+
+            TextBox txtMaSV = new TextBox();
+            txtMaSV.Location = new Point(30, 90);
+            txtMaSV.Width = 250;
+
+            Label lblHoTen = new Label();
+            lblHoTen.Text = "Họ và tên";
+            lblHoTen.Location = new Point(30, 140);
+
+            TextBox txtHoTen = new TextBox();
+            txtHoTen.Location = new Point(30, 170);
+            txtHoTen.Width = 250;
+
+            Label lblNgaySinh = new Label();
+            lblNgaySinh.Text = "Ngày sinh";
+            lblNgaySinh.Location = new Point(30, 220);
+
+            DateTimePicker dtNgaySinh = new DateTimePicker();
+            dtNgaySinh.Location = new Point(30, 250);
+            dtNgaySinh.Width = 250;
+
+            Label lblGioiTinh = new Label();
+            lblGioiTinh.Text = "Giới tính";
+            lblGioiTinh.Location = new Point(30, 300);
+
+            ComboBox cboGioiTinh = new ComboBox();
+            cboGioiTinh.Location = new Point(30, 330);
+            cboGioiTinh.Width = 250;
+
+            cboGioiTinh.Items.Add("Nam");
+            cboGioiTinh.Items.Add("Nữ");
+
+            Label lblLop = new Label();
+            lblLop.Text = "Lớp";
+            lblLop.Location = new Point(30, 380);
+
+            ComboBox cboLop = new ComboBox();
+            cboLop.Location = new Point(30, 410);
+            cboLop.Width = 250;
+
+            cboLop.Items.Add("68PM1");
+            cboLop.Items.Add("68PM2");
+
+            Button btnThem = new Button();
+            btnThem.Text = "Thêm";
+            btnThem.Location = new Point(30, 500);
+
+            Button btnSua = new Button();
+            btnSua.Text = "Sửa";
+            btnSua.Location = new Point(120, 500);
+
+            Button btnXoa = new Button();
+            btnXoa.Text = "Xóa";
+            btnXoa.Location = new Point(210, 500);
+
+            DataGridView dgv = new DataGridView();
+
+            dgv.Location = new Point(350, 60);
+            dgv.Size = new Size(700, 400);
+
+            dgv.ColumnCount = 5;
+
+            dgv.Columns[0].Name = "Mã SV";
+            dgv.Columns[1].Name = "Họ tên";
+            dgv.Columns[2].Name = "Ngày sinh";
+            dgv.Columns[3].Name = "Giới tính";
+            dgv.Columns[4].Name = "Lớp";
+
+            dgv.Rows.Add("1", "Nguyễn Văn A", "11/03/2026", "Nam", "68PM1");
+
+            btnThem.Click += (s, e) =>
+            {
+                dgv.Rows.Add(
+                    txtMaSV.Text,
+                    txtHoTen.Text,
+                    dtNgaySinh.Text,
+                    cboGioiTinh.Text,
+                    cboLop.Text
+                );
+            };
+
+            btnXoa.Click += (s, e) =>
+            {
+                if (dgv.CurrentRow != null)
+                {
+                    dgv.Rows.Remove(dgv.CurrentRow);
+                }
+            };
+
+            this.Controls.Add(lblMaSV);
+            this.Controls.Add(txtMaSV);
+
+            this.Controls.Add(lblHoTen);
+            this.Controls.Add(txtHoTen);
+
+            this.Controls.Add(lblNgaySinh);
+            this.Controls.Add(dtNgaySinh);
+
+            this.Controls.Add(lblGioiTinh);
+            this.Controls.Add(cboGioiTinh);
+
+            this.Controls.Add(lblLop);
+            this.Controls.Add(cboLop);
+
+            this.Controls.Add(btnThem);
+            this.Controls.Add(btnSua);
+            this.Controls.Add(btnXoa);
+
+            this.Controls.Add(dgv);
+        }
+
+        private void quảnLýSinhViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýLớpHọcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
